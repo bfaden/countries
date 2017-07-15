1b21856a582e128f5a010ada5a87fe01b12d0628
Ben Faden <bfaden@gmail.com>
2017-07-15 17:20:40 -0500

    Created the view

--- endpoint/router.php
+++ endpoint/router.php
@@ -3,9 +3,13 @@ require_once __DIR__ . '/vendor/autoload.php';
 
 const RESTCOUNTRIES_URL = "https://restcountries.eu/rest/v2/";
 
+if (preg_match('#^/web/#', $_SERVER["REQUEST_URI"])) {
+	return false;    // serve the requested resource as-is.
+}
+
 $klein = new \Klein\Klein();
 
-$klein->respond('GET', '/?[:name]?', function ($request, $response) {
+$klein->respond('/?[:name]?', function ($request, $response) {
 	$countryName = $request->name ? $request->name : '';
 	
 	$results = $countryName !== ''
